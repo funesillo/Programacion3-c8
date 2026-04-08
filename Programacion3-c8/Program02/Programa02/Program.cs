8faedbcd2b35e7b335f78083e8b0f71deb89145c
funesillo <98990542+funesillo@users.noreply.github.com>
2026-04-08 19:32:18 -0300

    Add files via upload
    Se generan los primeros files para el primer trabajo practico de la materia

---
+++ Programacion3-c8/Program02/Programa02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Programa02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("---Bloque 1: Año de Cursado ---");
+
+        // Declaración de un entero
+        int año = 2026;
+
+        Console.WriteLine("1. TIPO ENTERO (int):");
+        Console.WriteLine("   Se usa para representar numeros como, edad, contadores o años.");
+        Console.WriteLine("   Valor guardado (Año actual): " + año);
+
+        Console.WriteLine("\nPresiona ENTER para cerrar.");
+        Console.ReadLine();
+
+
+    }
+}
